c28908db8a797f6cfadbb305e3efb4f79ae3b31b
WaddyAgnew <waddyagnew@gmail.com>
2023-11-09 18:39:25 -0500

    js

--- static/scripts/responses.js
+++ static/scripts/responses.js
@@ -1,21 +1,19 @@
+function getBotResponse(input) {
+    if (input == "rock"){
+        return "paper";
+    }else if(input == "paper") {
+        return "scissors";
+    }else if(input == "scissors") {
+        return "rock";
+    }
 
+    // other responses
+    if (input == "hello") {
+        return "hello there!";
+    }else if(input == "goodbye"){
+        return "Talk to you later";
+    }else {
+        return "Try asking something else..."
+    }
 
-
-var coll = document.createElementdByClassName("collapsible");
-
-for (let i =  0; i< coll.length; i++) {
-    coll[i].addEventListener("click",function(){
-        this.classList.toggle("active");
-
-        var content = this.nextElementSibling;
-
-        if (content.style.maxHeight){
-            content.style.maxHeight = null;
-        } else{
-            content.style.maxHeight = content.scrollHeight + "px";
-        }
-
-    });
 }
-
-// contiue
